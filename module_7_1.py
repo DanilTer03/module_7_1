395ac6dca5ab68b188d651035c5622f587eda639
Danil Terentev <danil.terentyev@gmail.com>
2024-08-12 12:13:00 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -5,7 +5,7 @@ class Product:
         self.category = category
 
     def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}'
+        return f"{self.name}, {self.weight}, {self.category}"
 
 
 class Shop:
@@ -13,20 +13,27 @@ class Shop:
         self.__file_name = 'products.txt'
 
     def get_products(self):
-        with open(self.__file_name, 'r', encoding='utf8') as file:
-            return file.read()
+        file = open(self.__file_name, 'r', encoding='utf-8')
+        content = file.read()
+        file.close()
+        return content
 
     def add(self, *products):
-        available_products = self.get_products()
+        product_names = []
 
-        product_names = [line.split(',')[0].strip() for line in available_products.splitlines()]
+        available_products = self.get_products()
+        lines = available_products.splitlines()  # Разделяем строки
+        for line in lines:
+            name = line.split(',')[0].strip()  # Получаем имя продукта - первая позиция,разделитель запятая
+            product_names.append(name)
 
         for product in products:
             if product.name in product_names:
                 print(f'Продукт {product.name} уже есть в магазине')
             else:
-                with open(self.__file_name, 'a', encoding='utf8') as file:
-                    file.write(str(product) + '\n')
+                file = open(self.__file_name, 'a', encoding='utf-8')
+                file.write(str(product) + '\n')
+                file.close()
 
 
 s1 = Shop()
@@ -39,3 +46,4 @@ print(p2)  # __str__
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
+
